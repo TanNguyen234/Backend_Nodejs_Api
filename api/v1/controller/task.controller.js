24ba72a5d94189b32cd0d3e8c98c853ea58d762f
TanNguyen234 <tanntd.2005@gmail.com>
2024-09-20 18:03:09 +0700

    Phan trang

--- api/v1/controller/task.controller.js
+++ api/v1/controller/task.controller.js
@@ -1,22 +1,33 @@
-const taskModel = require('../models/task.model');
+const Task = require('../models/task.model');
+const paginationHelper = require('../../../helpers/pagination');
 
 //[GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
+    //Bộ lọc trạng thái
     const find = {
         deleted: false,
     }
 
-    const sort = {}
-
     if(req.query.status) {
         find.status = req.query.status
     }
+    //Pagination
+    const countTasks = await Task.countDocuments(find); // Hàm count trong mongoose để tổng số sản phẩm
+
+    let objectPagination = paginationHelper({
+        currentPage: 1,
+        limitItem: 2
+    }, req.query, countTasks)
+    //Sort
+    const sort = {}
 
     if(req.query.sortKey && req.query.sortValue) {
         sort[req.query.sortKey] = req.query.sortValue; //Vì key, value là linh động
     }
+    //End Sort
 
-    const tasks = await taskModel.find(find).sort(sort)
+    const tasks = await Task.find(find).sort(sort).limit(objectPagination.limitItem).skip(objectPagination.skip);
+
     res.json(tasks)
 };
 
