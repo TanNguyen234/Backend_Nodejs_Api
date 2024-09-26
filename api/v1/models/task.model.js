ce2394d19b89873ce72f359947600d7b85540b8a
TanNguyen234 <tanntd.2005@gmail.com>
2024-09-26 15:15:14 +0700

    Luu id của user tao task

--- api/v1/models/task.model.js
+++ api/v1/models/task.model.js
@@ -7,6 +7,7 @@ const taskSchema = new mongoose.Schema({//Thiết lập schema
   content: String,
   timeStart: Date,
   timeFinish: Date,
+  createdBy: String,
   deleted: {
     type: Boolean,
     default: false
