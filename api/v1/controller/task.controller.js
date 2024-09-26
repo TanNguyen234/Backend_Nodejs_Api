29813c94942ef70606d75ee2dd851731c09784f2
TanNguyen234 <tanntd.2005@gmail.com>
2024-09-26 15:35:40 +0700

    Lay ra danh sach stask theo dung user

--- api/v1/controller/task.controller.js
+++ api/v1/controller/task.controller.js
@@ -6,6 +6,10 @@ const searchHelper = require('../../../helpers/search');
 module.exports.index = async (req, res) => {
     //Bộ lọc trạng thái
     const find = {
+        $or: [
+            { createdBy: req.user.id },
+            { listUser: req.user.id }
+        ],
         deleted: false,
     }
 
